7aebb9ba5049d0c5d487ce246315d2d40738cd7c
jianghang <jianghang@didiglobal.com>
2022-01-12 20:07:00 +0800

    Add github contribution calendar

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 ## GitHub contribution calendar
-![github calendar]()
+<!--START_SECTION:github_calendar-->
+![github_calendar](https://s2.loli.net/2022/01/12/ym2xPeMjLqD8AcX.png)
+<!--END_SECTION:github_calendar-->
 
 ## Leetcode summary
-![Leetcode summary](https://s2.loli.net/2022/01/12/r9xK5qCfUSgXe3a.png)
+<!--START_SECTION:leetcode_summary-->
+![leetcode_summary](https://s2.loli.net/2022/01/12/yVNLpibhXM58lJR.png)
+<!--END_SECTION:leetcode_summary-->
 
 ## TODO
-- [ ] 生成 github 提交记录图
-- [ ] add cache for pip and playwright
+- [x] 生成 github 提交记录图
+- [x] add cache for pip and playwright
--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from datetime import datetime
 
 import httpx
 from playwright.async_api import async_playwright, Playwright, Page
@@ -11,7 +12,46 @@ if os.environ.get("IN_GH_ACTION"):
   DEBUG = False
 
 
-async def login(page: Page, username: str, password: str) -> bool:
+async def upload_image(path: str, token: str) -> str:
+  print(f"Uploading {path} to sm.ms")
+  async with httpx.AsyncClient(trust_env=False) as client:
+    resp = await client.post(
+      "https://sm.ms/api/v2/upload",
+      headers={"Authorization": token},
+      files={
+        "smfile": open(path, "rb"),
+      },
+      timeout=10,
+    )
+    resp.raise_for_status()
+    resp_data = resp.json()
+    if resp_data["success"] is False and resp_data["code"] == "image_repeated":
+      print(f"Image {path} already exists")
+      return resp_data["images"]
+    else:
+      url = resp_data["data"]["url"]
+      print(f"Uploaded: {url}")
+    return url
+
+
+async def delete_old_images(token: str):
+  print("Deleting old images")
+  async with httpx.AsyncClient(trust_env=False) as client:
+    resp = await client.get(
+      "https://sm.ms/api/v2/upload_history",
+      headers={"Authorization": token},
+    )
+    resp.raise_for_status()
+    history = resp.json()["data"]
+    now = datetime.now()
+    for item in history:
+      created_at = datetime.fromtimestamp(item["created_at"])
+      if (now - created_at).days >= 7:
+        await client.get(item["delete"])
+        print(f"Deleted image {item['filename']}")
+
+
+async def login_leetcode(page: Page, username: str, password: str) -> bool:
   await page.goto(LEETCODE_BASE)
 
   # wait for the popup
@@ -22,36 +62,24 @@ async def login(page: Page, username: str, password: str) -> bool:
   await page.click('button:has-text("登录")')
   await page.wait_for_timeout(500)
 
-  # test if login succeed
+  # test if login_leetcode succeed
   cookies = await page.context.cookies(LEETCODE_BASE)
   for cookie in cookies:
     if cookie["name"] == "LEETCODE_SESSION":
-      print(f"Logged in as {username}")
+      print(f"Logged in leetcode as {username}")
       return True
-  print(f"Login as {username} failed")
+  print(f"Login leetcode as {username} failed")
   return False
 
 
 async def clip_leetcode_summary_page(
-    playwright: Playwright,
+    page: Page,
     username: str,
     password: str,
     save_to: str,
 ) -> bool:
-  print("Launching firefox browser")
-  if DEBUG:
-    browser = await playwright.firefox.launch(headless=False, slow_mo=500)
-  else:
-    browser = await playwright.firefox.launch()
-  context = await browser.new_context(
-    viewport={"width": 1920, "height": 1080},
-    screen={"width": 1920, "height": 1080},
-    device_scale_factor=2,
-  )
-  page = await context.new_page()
-
-  print(f"Logging in as {username}")
-  if not await login(page, username, password):
+  print(f"Logging in leetcode as {username}")
+  if not await login_leetcode(page, username, password):
     return False
 
   await page.goto(f"{LEETCODE_BASE}/u/{username}/")
@@ -61,54 +89,77 @@ async def clip_leetcode_summary_page(
     path=save_to,
     clip=dict(x=700, y=160, width=772, height=365)
   )
-  await browser.close()
   return True
 
 
-async def upload_image(path: str):
-  print(f"Uploading {path} to sm.ms")
-  async with httpx.AsyncClient(trust_env=False) as client:
-    resp = await client.post(
-      "https://sm.ms/api/v2/upload",
-      files={"smfile": open(path, "rb")},
-      timeout=10,
-    )
-    resp.raise_for_status()
-    url = resp.json()["data"]["url"]
-    print(f"Uploaded: {url}")
-    return url
+async def clip_github_calendar(
+    page: Page, username: str, save_to: str,
+) -> bool:
+  print("Clipping github calendar")
+  await page.goto(f"https://github.com/{username}")
+  await page.wait_for_timeout(500)
+  calendar = page.locator("div.js-yearly-contributions")
+  await calendar.screenshot(path=save_to)
+  return True
 
 
 def update_readme_links(links: dict):
-  with open("./README.md.in", "rt") as f:
-    content = f.read()
-  content = content.format_map(links)
+  with open("./README.md", "rt") as f:
+    content = f.readlines()
+
+  for name, link in links.items():
+    i = content.index(f"<!--START_SECTION:{name}-->\n")
+    j = content.index(f"<!--END_SECTION:{name}-->\n")
+    content[i+1:j] = [f"![{name}]({link})\n"]
+
   with open("./README.md", "wt") as f:
-    f.write(content)
+    f.write("".join(content))
   print("Updated README.md")
 
 
 async def run() -> bool:
-  username = os.environ["LC_USERNAME"]
-  password = os.environ["LC_PASSWORD"]
+  lc_username = os.environ["LC_USERNAME"]
+  lc_password = os.environ["LC_PASSWORD"]
+  gh_username = os.environ["GH_USERNAME"]
+  sm_token = os.environ["SM_TOKEN"]
+
   output_path = "./output"
   os.makedirs(output_path, exist_ok=True)
 
   leetcode_image = os.path.join(output_path, "leetcode_summary.png")
-  async with async_playwright() as playwright:
-    clipped = await clip_leetcode_summary_page(
-      playwright, username, password, leetcode_image
-    )
-    if not clipped:
-      return False
+  github_image = os.path.join(output_path, "github_calendar.png")
 
-  leetcode_url = await upload_image(leetcode_image)
-  update_readme_links(
-    {
-      "leetcode_summary_image_url": leetcode_url,
-      "github_calendar_url": "",
-    }
-  )
+  async with async_playwright() as playwright:
+    print("Launching firefox browser")
+    if DEBUG:
+      browser = await playwright.firefox.launch(headless=False, slow_mo=500)
+    else:
+      browser = await playwright.firefox.launch()
+
+    context = await browser.new_context(
+      viewport={"width": 1920, "height": 1080},
+      screen={"width": 1920, "height": 1080},
+      device_scale_factor=2,
+    )
+    page = await context.new_page()
+    links = {}
+    if await clip_leetcode_summary_page(
+        page, lc_username, lc_password, leetcode_image
+    ):
+      leetcode_url = await upload_image(leetcode_image, sm_token)
+      links["leetcode_summary"] = leetcode_url
+    if await clip_github_calendar(
+        page, gh_username, github_image
+    ):
+      github_url = await upload_image(github_image, sm_token)
+      links["github_calendar"] = github_url
+
+  if not links:
+    print("No links to update")
+    return False
+
+  update_readme_links(links)
+  await delete_old_images(sm_token)
   return True
 
 
