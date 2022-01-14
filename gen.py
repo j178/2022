25b30deb6e4e24e8ed126b00b4d0679e60e372d2
jianghang <jianghang@didiglobal.com>
2022-01-14 15:16:56 +0800

    Refactor data generators

--- gen.py
+++ gen.py
@@ -1,3 +1,4 @@
+import abc
 import asyncio
 import json
 import os
@@ -9,18 +10,29 @@ from playwright.async_api import async_playwright, Page
 
 LEETCODE_BASE = "https://leetcode-cn.com"
 TZ = ZoneInfo("Asia/Shanghai")
+OUTPUT_FOLDER = "./output"
 DEBUG = True
 
 if os.environ.get("CI"):
   DEBUG = False
 
 
-async def upload_image(path: str, token: str) -> str:
-  print(f"Uploading {path} to sm.ms")
-  async with httpx.AsyncClient(trust_env=False) as client:
-    resp = await client.post(
-      "https://sm.ms/api/v2/upload",
-      headers={"Authorization": token},
+class ImageService:
+  name = "sm.ms"
+  base_url = "https://sm.ms/api/v2"
+
+  def __init__(self, credential):
+    self.credential = credential
+    self.client = httpx.AsyncClient(
+      base_url=self.base_url,
+      trust_env=False,
+      headers={"Authorization": self.credential},
+    )
+
+  async def upload(self, path: str) -> str:
+    print(f"Uploading {path} to {self.name}")
+    resp = await self.client.post(
+      "/upload",
       files={
         "smfile": open(path, "rb"),
       },
@@ -36,106 +48,157 @@ async def upload_image(path: str, token: str) -> str:
       print(f"Uploaded: {url}")
     return url
 
-
-async def delete_old_images(token: str):
-  print("Deleting old images")
-  async with httpx.AsyncClient(trust_env=False) as client:
-    resp = await client.get(
-      "https://sm.ms/api/v2/upload_history",
-      headers={"Authorization": token},
-    )
+  async def cleanup(self) -> None:
+    print("Deleting old images")
+    resp = await self.client.get("/upload_history")
     resp.raise_for_status()
     history = resp.json()["data"]
     now = datetime.now()
     for item in history:
       created_at = datetime.fromtimestamp(item["created_at"])
+      filename = item["filename"]
       if (now - created_at).days >= 7:
-        await client.get(item["delete"])
-        print(f"Deleted image {item['filename']}")
+        try:
+          await self.client.get(item["delete"])
+          print(f"Deleted image {filename}")
+        except httpx.HTTPError as e:
+          print(f"Delete image {filename} failed: {e!r}")
 
 
-async def login_leetcode(page: Page, username: str, password: str) -> bool:
-  await page.goto(LEETCODE_BASE)
-
-  # wait for the popup
-  await page.click("text=帐号密码登录")
-  await page.click('[placeholder="手机/邮箱"]')
-  await page.fill('[placeholder="手机/邮箱"]', username)
-  await page.fill('[placeholder="输入密码"]', password)
-  await page.click('button:has-text("登录")')
-  await page.wait_for_timeout(500)
-
-  # test if login_leetcode succeed
-  cookies = await page.context.cookies(LEETCODE_BASE)
-  for cookie in cookies:
-    if cookie["name"] == "LEETCODE_SESSION":
-      print(f"Logged in leetcode as {username}")
-      return True
-  print(f"Login leetcode as {username} failed")
-  return False
+def get_today() -> str:
+  return datetime.now(timezone.utc).astimezone(TZ).strftime("%Y-%m-%d")
 
 
-async def clip_leetcode_summary_page(
-    page: Page,
-    username: str,
-    password: str,
-    save_to: str,
-):
-  print(f"Logging in leetcode as {username}")
-  if not await login_leetcode(page, username, password):
-    raise Exception("Login failed")
+class DataGenerator:
+  name: str
 
-  await page.goto(f"{LEETCODE_BASE}/u/{username}/")
-  await page.wait_for_timeout(2000)
-
-  await page.screenshot(
-    path=save_to,
-    clip=dict(x=700, y=160, width=772, height=365)
-  )
+  @abc.abstractmethod
+  async def generate(self) -> dict[str: str]:
+    raise NotImplementedError
 
 
-async def clip_github_calendar(
-    page: Page, username: str, save_to: str,
-):
-  print("Clipping github calendar")
-  await page.goto(f"https://github.com/{username}")
-  await page.wait_for_timeout(500)
-  calendar = page.locator("div.js-yearly-contributions")
-  await calendar.screenshot(path=save_to)
+class LeetcodeSummary(DataGenerator):
+  name = "leetcode_summary"
+
+  def __init__(self, page: Page, username: str, password: str, image_service: ImageService):
+    self.page = page
+    self.username = username
+    self.password = password
+    self.image_service = image_service
+
+  async def login_leetcode(self) -> bool:
+    print(f"Logging in leetcode")
+    page = self.page
+    await page.goto(LEETCODE_BASE)
+
+    # wait for the popup
+    await page.click("text=帐号密码登录")
+    await page.click('[placeholder="手机/邮箱"]')
+    await page.fill('[placeholder="手机/邮箱"]', self.username)
+    await page.fill('[placeholder="输入密码"]', self.password)
+    await page.click('button:has-text("登录")')
+    await page.wait_for_timeout(500)
+
+    # test if login_leetcode succeed
+    cookies = await page.context.cookies(LEETCODE_BASE)
+    for cookie in cookies:
+      if cookie["name"] == "LEETCODE_SESSION":
+        print(f"Logged in leetcode")
+        return True
+    print(f"Login leetcode failed")
+    return False
+
+  async def generate(self) -> dict[str: str]:
+    if not await self.login_leetcode():
+      raise Exception("Login failed")
+
+    await self.page.goto(f"{LEETCODE_BASE}/u/{self.username}/")
+    await self.page.wait_for_timeout(2000)
+
+    save_to = os.path.join(OUTPUT_FOLDER, f"{self.name}.png")
+    await self.page.screenshot(
+      path=save_to,
+      clip=dict(x=700, y=160, width=772, height=365)
+    )
+
+    image_url = await self.image_service.upload(save_to)
+    return {
+      self.name: image_url,
+      f"{self.name}_update_date": get_today(),
+    }
 
 
-async def login_geek_time(page: Page, phone: str, password: str) -> bool:
-  await page.goto("https://account.geekbang.org/login?country=86")
-  await page.wait_for_timeout(500)
+class GithubCalendar(DataGenerator):
+  name = "github_calendar"
 
-  await page.wait_for_selector('[placeholder="密码"]')
-  await page.fill('[placeholder="手机号"]', phone)
-  await page.fill('[placeholder="密码"]', password)
-  await page.check('input[type="checkbox"]')
-  await page.click(':nth-match(:text("登录"), 3)')
-  await page.wait_for_url("https://time.geekbang.org/")
+  def __init__(self, page: Page, username: str, image_service: ImageService):
+    self.page = page
+    self.username = username
+    self.image_service = image_service
 
-  # test if login_leetcode succeed
-  cookies = await page.context.cookies("https://time.geekbang.org")
-  for cookie in cookies:
-    if cookie["name"] == "GCESS":
-      print(f"Logged in geek time")
-      return True
-  print(f"Login geek time failed")
-  return False
+  async def generate(self) -> dict[str: str]:
+    page = self.page
+    await page.goto(f"https://github.com/{self.username}")
+    await page.wait_for_timeout(500)
+    calendar = page.locator("div.js-yearly-contributions")
+
+    save_to = os.path.join(OUTPUT_FOLDER, f"{self.name}.png")
+    await calendar.screenshot(path=save_to)
+
+    image_url = await self.image_service.upload(save_to)
+    return {
+      self.name: image_url,
+      f"{self.name}_update_date": get_today(),
+    }
 
 
-async def clip_geek_time_calendar(
-    page: Page, username: str, password: str, save_to: str,
-):
-  print("Clipping geek time calendar")
-  if not await login_geek_time(page, username, password):
-    raise Exception("Login failed")
+class GeekTimeCalendar(DataGenerator):
+  name = "geek_time_calendar"
 
-  await page.goto("https://time.geekbang.org/dashboard/usercenter")
-  await page.wait_for_timeout(500)
-  calendar = page.locator("div[class^=LearningRecord_learningWrapper]")
-  await calendar.screenshot(path=save_to)
+  def __init__(self, page: Page, phone: str, password: str, image_service: ImageService):
+    self.page = page
+    self.phone = phone
+    self.password = password
+    self.image_service = image_service
+
+  async def login_geek_time(self) -> bool:
+    page = self.page
+    await page.goto("https://account.geekbang.org/login?country=86")
+    await page.wait_for_timeout(500)
+
+    await page.wait_for_selector('[placeholder="密码"]')
+    await page.fill('[placeholder="手机号"]', self.phone)
+    await page.fill('[placeholder="密码"]', self.password)
+    await page.check('input[type="checkbox"]')
+    await page.click(':nth-match(:text("登录"), 3)')
+    await page.wait_for_url("https://time.geekbang.org/")
+
+    # test if login_leetcode succeed
+    cookies = await page.context.cookies("https://time.geekbang.org")
+    for cookie in cookies:
+      if cookie["name"] == "GCESS":
+        print(f"Logged in geek time")
+        return True
+    print(f"Login geek time failed")
+    return False
+
+  async def generate(self) -> dict[str: str]:
+    page = self.page
+    if not await self.login_geek_time():
+      raise Exception("Login failed")
+
+    await page.goto("https://time.geekbang.org/dashboard/usercenter")
+    await page.wait_for_timeout(500)
+    calendar = page.locator("div[class^=LearningRecord_learningWrapper]")
+
+    save_to = os.path.join(OUTPUT_FOLDER, f"{self.name}.png")
+    await calendar.screenshot(path=save_to)
+
+    image_url = await self.image_service.upload(save_to)
+    return {
+      self.name: image_url,
+      f"{self.name}_update_date": get_today(),
+    }
 
 
 def update_readme(params: dict):
@@ -169,9 +232,7 @@ async def run() -> bool:
   os.makedirs(output_path, exist_ok=True)
   os.makedirs(debug_path, exist_ok=True)
 
-  leetcode_image = "leetcode_summary.png"
-  github_image = "github_calendar.png"
-  geek_time_image = "geek_time_calendar.png"
+  image_service = ImageService(sm_token)
 
   async with async_playwright() as playwright:
     print("Launching firefox browser")
@@ -186,51 +247,30 @@ async def run() -> bool:
       device_scale_factor=2,
     )
     page = await context.new_page()
-    data = {}
-    today = datetime.now(timezone.utc).astimezone(TZ).strftime("%Y-%m-%d")
 
-    try:
-      save_path = os.path.join(output_path, leetcode_image)
-      await clip_leetcode_summary_page(
-          page, lc_username, lc_password, save_path
-      )
-      leetcode_url = await upload_image(save_path, sm_token)
-      data["leetcode_summary"] = leetcode_url
-      data["leetcode_update_date"] = today
-    except Exception as e:
-      print(f"::error::Clip leetcode summary error: {e!r}")
-      await page.screenshot(path=os.path.join(debug_path, leetcode_image))
+    sources = [
+      LeetcodeSummary(page, lc_username, lc_password, image_service),
+      GithubCalendar(page, gh_username, image_service),
+      GeekTimeCalendar(page, gt_username, gt_password, image_service),
+    ]
+    full_data = {}
+    for source in sources:
+      print(f"Generating {source.name}")
+      try:
+        data = await source.generate()
+        full_data.update(data)
+        print(f"Generated {source.name}")
+      except Exception as e:
+        print(f"::error::Generate {source.name} failed: {e!r}")
+        await page.screenshot(path=os.path.join(debug_path, f"{source.name}.png"))
 
-    try:
-      save_path = os.path.join(output_path, github_image)
-      await clip_github_calendar(
-          page, gh_username, save_path
-      )
-      github_url = await upload_image(save_path, sm_token)
-      data["github_calendar"] = github_url
-      data["github_update_date"] = today
-    except Exception as e:
-      print(f"::error::Clip github calendar error: {e!r}")
-      await page.screenshot(path=os.path.join(debug_path, github_image))
-
-    try:
-      save_path = os.path.join(output_path, geek_time_image)
-      await clip_geek_time_calendar(
-        page, gt_username, gt_password, save_path
-      )
-      geek_time_url = await upload_image(save_path, sm_token)
-      data["geek_time_calendar"] = geek_time_url
-      data["geek_time_update_date"] = today
-    except Exception as e:
-      print(f"::error::Clip geek time calendar error: {e!r}")
-      await page.screenshot(path=os.path.join(debug_path, geek_time_image))
-
-  if not data:
+  if not full_data:
     print("::error::No links to update")
     return False
 
-  update_readme(data)
-  await delete_old_images(sm_token)
+  update_readme(full_data)
+  await image_service.cleanup()
+  await image_service.client.aclose()
   return True
 
 
